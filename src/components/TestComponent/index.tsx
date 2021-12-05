962147a696e8c98ce77746af965ec8279ddd97e4
eliasdbr <eliasdbr@outlook.com>
2021-12-04 21:48:49 -0300

    eliasdbr/new_component_template

---
+++ src/components/TestComponent/index.tsx
@@ -0,0 +1,15 @@
+import React /*, { useState }*/ from 'react';
+
+function TestComponent( /*{ prop1,prop2,prop3... }*/ ): JSX.Element {
+	// States definitions
+	// const [state,setState] = useState<string>('default_value');
+	
+	// Render component
+	return (
+		<>
+			<h2>TestComponent</h2>
+		</>
+	);
+} 
+
+export default TestComponent;
